7494e7cb81776673db3cc3bc801c5682d4ddd01e
igor-pronin <gosha.pronin.2000@icloud.com>
2019-12-26 00:49:52 +0300

    please offset

---
+++ src/List/List/List.h
@@ -0,0 +1,398 @@
+#pragma once
+#include<iostream>
+using namespace std;
+template<class TKey, class TData>
+class TList
+{
+public:
+	struct TNode
+	{
+		TKey key;
+		TData *pData;
+		TNode *pNext;
+	};
+public:
+		TNode *pFirst;
+		TNode *pPrev;
+		TNode *pNext;
+		TNode *pCur;
+public:
+	TList();
+	TList(const TList<TKey, TData>&);
+	TList(const TNode&);
+	~TList();
+	TNode* Search(TKey);
+	void InsertStart(TKey, TData*);
+	void InsertEnd(TKey, TData*);
+	void InsertBefore(TKey, TKey, TData*);
+	void InsertAfter(TKey, TKey, TData*);
+	void Delete(TKey);
+	void Reset();
+	bool IsEnded()const;
+	void Next();
+	friend ostream& operator <<(ostream& o, TList<TKey, TData> &list)
+	{
+		if (list.pFirst == nullptr)
+		{
+			throw "Empty list";
+		}
+		TNode* tmp0 = list.pCur;
+		TNode* tmp1 = list.pPrev;
+		TNode* tmp2 = list.pNext;
+		list.Reset();
+		while (list.pCur != nullptr)
+		{
+			o << list.pCur->key << " " << *list.pCur->pData << endl;
+			list.Next();
+		}
+		list.pCur = tmp0;
+		list.pPrev = tmp1;
+		list.pNext = tmp2;
+		return o;
+	}
+};
+
+template<class TKey, class TData>
+TList<TKey, TData >::TList()
+{
+	pFirst = nullptr;
+	pPrev = nullptr;
+	pCur = nullptr;
+	pNext = nullptr;
+}
+template<class TKey, class TData>
+TList<TKey, TData>::TList(const TList<TKey, TData>& list)
+{
+	if (list.pFirst == nullptr)
+		throw "empty list";
+	pFirst = new TNode;
+	pFirst->key = list.pFirst->key;
+	pFirst->pData = new TData;
+	*pFirst->pData = *list.pFirst->pData;
+	pFirst->pNext = list.pFirst->pNext;//
+	TNode* node = list.pFirst->pNext;
+	TNode* copy = pFirst;
+	while (node != nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = node->key;
+		tmp->pData = new TData;
+		*tmp->pData = *node->pData;
+		copy->pNext = tmp;
+		copy = tmp;
+		node = node->pNext;
+	}
+	Reset();
+}
+template<class TKey, class TData>
+TList<TKey, TData>::TList(const TList<TKey, TData>::TNode& node)
+{
+	if(node == nullptr)
+		throw"empty node";
+	pFirst = new TNode;
+	pFirst->key = node.key;
+	pFirst->pData = new TData;
+	*pFirst->pData = *node.pData;
+	TNode* _node = node.pNext;
+	TNode* copy = pFirst;
+	while (_node != nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = _node->key;
+		tmp->pData = new TData;
+		*tmp->pData = *_node->pData; 
+		copy->pNext = tmp;
+		copy = tmp;
+		_node = _node.pNext;
+	}
+	Reset();
+}
+template<class TKey, class TData>
+TList<TKey, TData>::~TList()
+{
+	while (!IsEnded())
+	{
+		Next();
+		delete[] pPrev;
+	}
+	delete[] pCur;
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::Reset()
+{
+	pPrev = nullptr;
+	pCur = pFirst;
+	if (pFirst != nullptr)
+		pNext = pCur->pNext;
+	else
+		pNext = nullptr;
+}
+template<class TKey, class TData>
+bool TList<TKey, TData>::IsEnded() const
+{
+	return(pCur == nullptr);
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::Next()
+{
+	if (IsEnded())
+		throw"cant complete Next";
+	pPrev = pCur;
+	pCur = pNext;
+	if (pNext != nullptr)
+		pNext = pNext->pNext;
+}
+template<class TKey, class TData>
+typename TList<TKey, TData>::TNode* TList<TKey, TData>::Search(TKey key)
+{
+	if (pFirst == nullptr)
+		return 0;
+	TNode* tmp0 = pCur;
+	TNode* tmp1 = pPrev;
+	TNode* tmp2 = pNext;
+	Reset();
+	while (!IsEnded() && (pCur->key != key))
+		Next();
+	if (pCur == nullptr)
+	{
+		pCur = tmp0;
+		pPrev = tmp1;
+		pNext = tmp2;
+		return 0;
+	}
+	TNode* tmp = pCur;
+	pCur = tmp0;
+	pPrev = tmp1;
+	pNext = tmp2;
+	return tmp;
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::InsertStart(TKey key, TData* data)
+{
+	if (pFirst == nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = key;
+		tmp->pData = new TData;
+		if (data != nullptr)
+			*tmp->pData = *data;
+		else
+			tmp->pData = nullptr;
+		tmp->pNext = nullptr;
+		pFirst = tmp;
+		pCur = pFirst;
+		return;
+	}
+	TNode* tmp = new TNode;
+	tmp->key = key;
+	tmp->pData = new TData;
+	if (data != nullptr)
+		*tmp->pData = *data;
+	else
+		tmp->pData = nullptr;
+	tmp->pNext = pFirst;
+	pFirst = tmp;
+	if (pCur == pFirst->pNext)
+		pPrev = pFirst;
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::InsertEnd(TKey key, TData* data)
+{
+	if (pFirst == nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = key;
+		tmp->pData = new TData;
+		*tmp->pData = *data;
+		tmp->pNext = nullptr;
+		pFirst = tmp;
+		pCur = pFirst;
+		return;
+	}
+	TNode* tmp0 = pCur;
+	TNode* tmp1 = pPrev;
+	TNode* tmp2 = pNext;
+	while (!IsEnded())
+		Next();
+	TNode* tmp = new TNode;
+	tmp->key = key;
+	tmp->pData = new TData;
+	*tmp->pData = *data;
+	pPrev->pNext = tmp;
+	tmp->pNext = nullptr;
+	if (tmp0 == pPrev)
+	{
+		pNext = pPrev->pNext;
+		pCur = tmp0;
+		pPrev = tmp1;
+	}
+	if (tmp0 == nullptr)
+	{
+		pPrev = pPrev->pNext;
+		pCur = tmp0;
+		pNext = tmp2;
+	}
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::InsertBefore(TKey key, TKey newkey, TData* data)
+{
+	if (pFirst == nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = newkey;
+		tmp->pData = new TData;
+		*tmp->pData = *data;
+		tmp->pNext = nullptr;
+		pFirst = tmp;
+		pCur = pFirst;
+		return;
+	}
+	TNode* tmp0 = pCur;
+	TNode* tmp1 = pPrev;
+	TNode* tmp2 = pNext;
+	Reset();
+	while (!IsEnded() && (pCur->key != key))
+		Next();
+	if (pCur == nullptr)
+		throw "key is not found";
+	TNode* tmp = new TNode;
+	tmp->key = newkey;
+	tmp->pData = new TData;
+	*tmp->pData = *data;
+	tmp->pNext = pCur;
+	pPrev->pNext = tmp;
+	if (pCur == tmp0)
+	{
+		pPrev = pPrev->pNext;
+		pNext = tmp2;
+	}
+	else
+	if (pPrev == tmp0)
+	{
+		pNext = pPrev->pNext;
+		pCur = tmp0;
+		pPrev = tmp1;
+	}
+	else
+	{
+		pCur = tmp0;
+		pPrev = tmp1;
+		pNext = tmp2;
+	}
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::InsertAfter(TKey key, TKey newkey, TData* data)
+{
+	if (pFirst == nullptr)
+	{
+		TNode* tmp = new TNode;
+		tmp->key = newkey;
+		tmp->pData = new TData;
+		*tmp->pData = *data;
+		tmp->pNext = nullptr;
+		pFirst = tmp;
+		pCur = pFirst;
+		return;
+	}
+	TNode* tmp0 = pCur;
+	TNode* tmp1 = pPrev;
+	TNode* tmp2 = pNext;
+	Reset();
+	while (!IsEnded() && (pCur->key != key))
+		Next();
+	if (pCur == nullptr)
+		throw "key is not found";
+	TNode* tmp = new TNode;
+	tmp->key = newkey;
+	tmp->pData = new TData;
+	*tmp->pData = *data;
+	tmp->pNext = pNext;
+	pCur->pNext = tmp;
+	if (pCur == tmp0)
+	{
+		pNext = pCur->pNext;
+		pPrev = tmp1;
+	}
+	else
+		if (pNext == tmp0)
+		{
+			pPrev = pCur->pNext;
+			pCur = tmp0;
+			pNext = tmp2;
+		}
+		else
+		{
+			pCur = tmp0;
+			pPrev = tmp1;
+			pNext = tmp2;
+		}
+}
+template<class TKey, class TData>
+void TList<TKey, TData>::Delete(TKey key)
+{
+	if (pFirst == nullptr)
+		throw "cant delete element from empty list";
+	TNode* tmp0 = pCur;
+	TNode* tmp1 = pPrev;
+	TNode* tmp2 = pNext;
+	Reset();
+	while (!IsEnded() && (pCur->key != key))
+		Next();
+	if (pCur == nullptr)
+		throw "key is not found";
+	if (pCur == pFirst)
+	{
+		if (tmp0 == pFirst)
+		{
+			delete pFirst;
+			pFirst = pNext;
+			pCur = pFirst;
+			if (pNext != nullptr)
+			pNext = pCur->pNext;
+		}
+		else
+		{
+			delete pFirst;
+			pFirst = pNext;
+			pCur = tmp0;
+			pPrev = tmp1;
+			pNext = tmp2;
+		}
+	}
+	else
+		if (pCur == tmp0)
+		{
+			pPrev->pNext = pNext;
+			delete pCur;
+			pCur = tmp2;
+			pPrev = tmp1;
+			if (tmp2 != 0)
+				pNext = tmp2->pNext;
+			else
+				pNext = tmp2;
+		}
+		else
+			if (pPrev == tmp0)
+			{
+				pPrev->pNext = pNext;
+				delete pCur;
+				pCur = tmp0;
+				pPrev = tmp1;
+				pNext = pNext;
+			}
+			else
+				if (pNext == tmp0)
+				{
+					pPrev->pNext = pNext;
+					delete pCur;
+					pCur = tmp0;
+					pNext = tmp2;
+				}
+				else
+				{
+					delete pCur;
+					pCur = tmp0;
+					pPrev = tmp1;
+					pNext = tmp2;
+				}
+}
---
+++ src/List/List/main.cpp
@@ -0,0 +1,100 @@
+#include "List.h"
+#include "VirtualStack.h"
+#include "Calculating.h"
+#include <iostream>
+int main()
+{
+	int a[1] = { 1 };
+	int b[1] = { 2 };
+	int c[1] = { 3 };
+	int d[1] = { 4 };
+		cout << "LIST TESTING" << endl;
+		TList<int, int> L1;
+		cout << "L1IsEnded" << " " << L1.IsEnded() << endl << endl;
+		L1.InsertStart(1, a);
+		TList<int, int> L2(L1);
+		cout << "L1InsertStart:" << endl << L1 << endl;
+		cout << "L2(L1):" << endl << L2 << endl;
+		L2.InsertEnd(2, b);
+		cout << "L2InsertEnd:" << endl << L2 << endl;
+		try
+		{
+			L2.InsertAfter(1, 3, c);
+		}
+		catch (const char* str)
+		{
+			cout << str;
+		}
+		cout << "L2InsertAfter:" << endl << L2 << endl;
+		try
+		{
+			L2.InsertBefore(2, 4, d);
+		}
+		catch (const char* str)
+		{
+			cout << str;
+		}
+		cout << "L2InsertBefore:" << endl << L2 << endl;
+		cout << "L2Search:" << " " << L2.Search(1)->key << " " << *L2.Search(1)->pData << endl << endl;
+		try
+		{
+			L2.Delete(1);
+		}
+		catch (const char* str)
+		{
+			cout << str;
+		}
+		cout << "L2Delete:" << endl << L2 << endl;
+		L2.Reset();
+		cout << "L2Reset:" << " " << L2.pCur->key << " " << *L2.pCur->pData << endl << endl;
+		try
+		{
+			L2.Next();
+		}
+		catch (const char* str)
+		{
+			cout << str;
+		}
+		cout << "L2Next:" << " " << L2.pCur->key << " " << *L2.pCur->pData << endl << endl;
+	string s;
+	string postfixform;
+	int tipe;
+	double postfixformvolue = 0;
+	int size = 0;
+	cout << "Enter string" << endl;
+	cin >> s;
+	cout << "Enter 1 to creat TArrayStack, enter 2 to create TListStack" << endl;
+	cin >> tipe;
+	Calculating Postfix(tipe, s.length());
+	try
+	{
+		postfixform = Postfix.create_postfix(s);
+	}
+	catch (const char* str)
+	{
+		cout << str << endl;
+	}
+	cout << "postfix_form:" << endl;
+	cout << postfixform << endl;
+	double *volue = new double[postfixform.length()];
+	char * uniqueoperands = new char[postfixform.length()];
+	try
+	{
+		Calculating::get_operands(postfixform, volue, uniqueoperands, size);
+	}
+	catch (const char* str)
+	{
+		cout << str << endl;
+	}
+	try
+	{
+		postfixformvolue = Postfix.calculate(postfixform, volue, uniqueoperands, size);
+	}
+	catch (const char* str)
+	{
+		cout << str << endl;
+	}
+	cout << "postfixform volue:" << endl;
+	cout << postfixformvolue;
+	system("pause");
+}
